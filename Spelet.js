c62635f695716c53bd331602704ba3a639505e5f
Biopubler <tor.karlsson@skola.taby.se>
2026-05-12 13:14:43 +0200

    Add files via upload
    Lägger in grundkod med instruktioner etc!

---
+++ Spelet.js
@@ -0,0 +1,7 @@
+const KOLUMNER = 28;
+const RADER    = 21;
+const CELL     = 20;
+
+const canvas  = document.getElementById('spelplan');
+const ctx     = canvas.getContext('2d');
+const overlay = document.getElementById('overlay');
